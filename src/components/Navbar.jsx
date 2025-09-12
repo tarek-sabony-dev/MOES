6d5932fb1808fc2e08060f60f3fecfb5dcdfe21b
pepars-Rashid <peparsrashid@gmail.com>
2025-09-12 21:56:44 +0300

    missed a className

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -128,7 +128,7 @@ function Navbar ({ menuItems }) {
                 > 
                   <a
                     href={item.href}
-                    className=""
+                    className="w-full h-fit px-6 py-3 rounded-lg border text-opacity-100 font-semibold base bg-transparent hover:bg-white hover:text-black transition-colors block text-center"
                     onClick={() => setIsOpen(false)}
                   >
                     {item.name}
